aa91bc8473bea4709e5f55fe56a3c15101ada76d
Sławomir Bryś <slawomir.brys@mbank.pl>
2016-10-18 17:29:52 +0200

    Adding new element to Todo list.

--- app/todos-list/todos-list.component.ts
+++ app/todos-list/todos-list.component.ts
@@ -9,9 +9,23 @@ import {ALL_TODOS} from './todos.service';
 })
 export class TodosListComponent implements OnInit {
     todos: Todo[];
+    newTitle: string;
+    newDescription: string;
+
     constructor() { }
 
     ngOnInit() {
         this.todos = ALL_TODOS;
+     }
+
+     add(){
+         let newItem : Todo = {
+            id: 0,
+            title: this.newTitle,
+            description: this.newDescription,
+            isFinished: false
+         };
+
+         this.todos.push(newItem);
      }
 }
